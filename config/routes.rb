61e16d5c0f511928b8f62df17deef12dc631d4c3
Milton Lash <iblash@live.com>
2023-05-01 16:25:40 +0000

    Created and migrated todo

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,23 @@ Rails.application.routes.draw do
 
 
 
+  # Routes for the Todo resource:
+
+  # CREATE
+  post("/insert_todo", { :controller => "todos", :action => "create" })
+          
+  # READ
+  get("/todos", { :controller => "todos", :action => "index" })
+  
+  get("/todos/:path_id", { :controller => "todos", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_todo/:path_id", { :controller => "todos", :action => "update" })
+  
+  # DELETE
+  get("/delete_todo/:path_id", { :controller => "todos", :action => "destroy" })
+
+  #------------------------------
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_05_01_162431) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string "content"
+    t.string "status"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
